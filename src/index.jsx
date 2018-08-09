640c11d1b3eb18fbe266b97cf5e12f47737b12a0
Di Coding <dicoding@yandex.ru>
2018-08-09 08:41:14 +0300

    use faker and js-cookie to set initial username

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/application.css';
-// import faker from 'faker';
+import faker from 'faker';
+import cookies from 'js-cookie';
+
 // import gon from 'gon';
-// import cookies from 'js-cookie';
 // import io from 'socket.io-client';
 import App from './components/App';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
+
+const username = cookies.get('name') || faker.name.findName();
+cookies.set('name', username);
 
 ReactDOM.render(<App initialState={gon} />, document.getElementById('chat'));
