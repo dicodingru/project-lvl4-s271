68cf8266a498b587dbb62b8d1b1bc60554fc22dd
Di Coding <dicoding@yandex.ru>
2018-08-12 10:14:10 +0300

    fix MessagesList component, fix App test

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import App from '../src/components/App';
 
 test('App', () => {
-  const wrapper = mount(
-    <App
-      initialState={{
-        channels: [
-          { id: 1, name: 'general', removable: false },
-          { id: 2, name: 'random', removable: false }
-        ],
-        messages: [
-          {id: 1, channelId: 1, username: 'user1', text: 'message1'},
-          {id: 2, channelId: 1, username: 'user2', text: 'message2'}
-        ]
-      }}
-    />
-  );
+  const initialState = {
+    channels: [
+      { id: 1, name: 'general', removable: false },
+      { id: 2, name: 'random', removable: false }
+    ],
+    messages: [
+      { id: 1, channelId: 1, username: 'user1', text: 'message1' },
+      { id: 2, channelId: 1, username: 'user2', text: 'message2' }
+    ]
+  };
+
+  const wrapper = mount(<App initialState={initialState} />);
 
   expect(wrapper.render()).toMatchSnapshot();
 });
