434184377c6a645fe011fb44d810ca1e9b84c3bc
Di Coding <dicoding@yandex.ru>
2018-08-21 09:28:15 +0300

    Show error when no internet connection

--- src/components/NewChannelForm.jsx
+++ src/components/NewChannelForm.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import actionCreators, { addChannel } from '../actions';
+import Error from './Error';
 
 const mapStateToProps = (state) => {
   const props = {
@@ -14,6 +15,13 @@ const mapStateToProps = (state) => {
 @connect(mapStateToProps, actionCreators)
 @reduxForm({ form: 'newChannel' })
 class NewChannelForm extends Component {
+  static propTypes = {
+    channelCreatingState: PropTypes.string.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    reset: PropTypes.func.isRequired,
+    createChannel: PropTypes.func.isRequired
+  };
+
   add = ({ name }) => {
     const { createChannel, reset } = this.props;
     const data = {
@@ -27,8 +35,10 @@ class NewChannelForm extends Component {
   render() {
     const { handleSubmit, channelCreatingState } = this.props;
     const disabled = channelCreatingState === 'requested';
+    const isError = channelCreatingState === 'failed';
     return (
       <form className="mb-4 w-100" onSubmit={handleSubmit(this.add)}>
+        {isError && <Error />}
         <div className="row d-flex align-items-center justify-content-between">
           <div className="col-9">
             <Field
