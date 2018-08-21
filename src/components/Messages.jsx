214155d8ac95a946893eddd22c788c1ba2ccff5b
Di Coding <dicoding@yandex.ru>
2018-08-21 07:55:41 +0300

    Render messages depending on current channel

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,43 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
 import MessagesList from './MessagesList';
 import NewMessageForm from './NewMessageForm';
 import UserContext from '../user-context';
 
-const mapStateToProps = (state) => {
-  const props = {
-    messages: state.messages,
-    currentChannelId: state.currentChannelId
-  };
-  return props;
-};
+const Messages = () => (
+  <div className="p-3 col-10 h-100 d-flex flex-column justify-content-end align-items-stretch">
+    <MessagesList />
+    <UserContext.Consumer>
+      {(username) => <NewMessageForm username={username} />}
+    </UserContext.Consumer>
+  </div>
+);
 
-@connect(mapStateToProps)
-export default class Messages extends Component {
-  static propTypes = {
-    messages: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        channelId: PropTypes.number,
-        text: PropTypes.string
-      })
-    )
-  };
-
-  static defaultProps = {
-    messages: []
-  };
-
-  render() {
-    const { messages } = this.props;
-    return (
-      <div className="p-3 col-10 h-100 d-flex flex-column justify-content-end align-items-stretch">
-        <MessagesList messages={messages} />
-        <UserContext.Consumer>
-          {(username) => <NewMessageForm username={username} />}
-        </UserContext.Consumer>
-      </div>
-    );
-  }
-}
+export default Messages;
