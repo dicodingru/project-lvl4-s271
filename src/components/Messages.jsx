ce631b1ee29e5e94af628141097faafcbf3e360e
Di Coding <dicoding@yandex.ru>
2018-08-30 23:52:27 +0300

    Fix firefox scrollable flex items bug

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -8,7 +8,7 @@ const Messages = () => (
   <Col
     xs="8"
     lg="9"
-    style={{ overflow: 'hidden' }}
+    style={{ overflow: 'hidden', maxHeight: '100%' }}
     className="d-flex flex-column justify-content-end">
     <MessagesList />
     <UserContext.Consumer>
